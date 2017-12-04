f5902d8b271e0ad887bc3f65bff4c253deb4dd76
kclark85 <32521814+kclark85@users.noreply.github.com>
2017-12-04 11:01:29 -0700

    added NextBool

---
+++ src/edu/msud/cs/cs1/NextBool.java
@@ -0,0 +1,29 @@
+package edu.msud.cs.cs1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class NextBool {
+
+    public static void main(String[] args) {
+        String inputStr = "1 0 true false True False TRUE FALSE 234 t f T F trUE fALsE";
+        Scanner input = new Scanner(inputStr);
+
+        // TODO: write your code here
+        try {
+            while (input.hasNext()) {
+                if(input.hasNextBoolean())
+                {
+                    System.out.println(input.nextBoolean());
+                }
+                else{
+                    System.out.println("Error: Null");
+                    input.next();
+                }
+            }
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
